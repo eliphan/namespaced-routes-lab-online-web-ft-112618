2a8ca412699f04379108786001f9af1d1f53ad3a
Elise Phan <aliceconnic@gmail.com>
2019-02-19 02:40:48 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     resources :songs, only: [:index, :show]
   end
   resources :songs
+  
   namespace '/admin' do
     resources :preferences, only: [:index]
   end
